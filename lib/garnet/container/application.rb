dc5130164182d40c0ea096f4d5c653ee45590784
Chi Man Lei <chimanlei@gmail.com>
2024-01-21 15:15:44 +0800

    minor code refactoring

--- lib/garnet/container/application.rb
+++ lib/garnet/container/application.rb
@@ -2,36 +2,36 @@
 
 module Garnet
   class Application < Container
-    def self.inherited(subclass)
-      super
-      setup_env(subclass)
-      setup_container_name(subclass, :@app_name)
-      setup_types(subclass)
-      setup_deps(subclass)
-      setup_autoload(subclass)
-      setup_component_dirs(subclass)
-      setup_inflector(subclass)
-      Garnet.app = subclass
-    end
-
     class << self
       attr_reader :app_name
-    end
 
-    def self.setup_component_dirs(subclass)
-      subclass.configure do |config|
-        config.component_dirs.add 'app' do |dir|
-          dir.namespaces.add_root const: container_name(subclass)
-        end
-        config.component_dirs.add 'lib/app' do |dir|
-          dir.auto_register = false
-          dir.namespaces.add_root const: container_name(subclass)
+      def inherited(subclass)
+        super
+        setup_env(subclass)
+        setup_container_name(subclass, :@app_name)
+        setup_types(subclass)
+        setup_deps(subclass)
+        setup_autoload(subclass)
+        setup_component_dirs(subclass)
+        setup_inflector(subclass)
+        Garnet.app = subclass
+      end
+
+      def setup_component_dirs(subclass)
+        subclass.configure do |config|
+          config.component_dirs.add 'app' do |dir|
+            dir.namespaces.add_root const: container_name(subclass)
+          end
+          config.component_dirs.add 'lib/app' do |dir|
+            dir.auto_register = false
+            dir.namespaces.add_root const: container_name(subclass)
+          end
         end
       end
-    end
 
-    def self.setup_inflector(klass)
-      klass.register('inflector', config.inflector)
+      def setup_inflector(klass)
+        klass.register('inflector', config.inflector)
+      end
     end
   end
 end
